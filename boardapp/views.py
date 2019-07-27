addbf5f6739d1bd53f0903f01e6a21da3d993685
kawaguchi <info@kawaguchi-design.com>
2019-07-27 23:37:31 +0900

    登録フォーム重複処理追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -4,8 +4,13 @@ from django.contrib.auth.models import User
 
 def signupfunc(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, 'mail@example.com', password)
-        return render(request, 'signup.html', {'some': 100})
+        username_post = request.POST['username']
+        password_post = request.POST['password']
+        try:
+            User.objects.get(username=username_post)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username_post, 'mail@example.com', password_post)
+            return render(request, 'signup.html', {'some': 100})
+
     return render(request, 'signup.html', {'some': 100})
