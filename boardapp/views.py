944640219a2c52dd43e5d64c33583f3740aae6e1
kawaguchi <info@kawaguchi-design.com>
2019-07-27 21:31:43 +0900

    ユーザー作成処理追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, 'mail@example.com', password)
+        return render(request, 'signup.html', {'some': 100})
     return render(request, 'signup.html', {'some': 100})
